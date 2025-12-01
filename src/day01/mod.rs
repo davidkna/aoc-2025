8d5b4153c3c00844666234ad43c1704dbf1eda60
David Knaack <davidkna@users.noreply.github.com>
2025-12-01 17:28:24 +0100

    day 01: cleanup

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -2,15 +2,17 @@ use bstr::ByteSlice;
 
 pub const INPUT: &[u8] = include_bytes!("../../inputs/day01.txt");
 
+fn parse_uint(s: &[u8]) -> i32 {
+    s.iter().fold(0, |acc, &c| acc * 10 + (c - b'0') as i32)
+}
+
 pub fn part_1(input: &[u8]) -> u32 {
     input
         .lines()
         .fold((50, 0), |(sum, zero_count), line| {
-            let dir = line[0];
-            let dist: i32 = line[1..].to_str().unwrap().parse().unwrap();
-            let out = match dir {
-                b'L' => sum - dist,
-                b'R' => sum + dist,
+            let out = match line {
+                [b'L', delta @ ..] => sum - parse_uint(delta),
+                [b'R', delta @ ..] => sum + parse_uint(delta),
                 _ => unreachable!(),
             }
             .rem_euclid(100);
@@ -20,12 +22,11 @@ pub fn part_1(input: &[u8]) -> u32 {
 }
 
 pub fn part_2(input: &[u8]) -> i32 {
-    // How often moves past 0
     input
         .lines()
         .fold((50, 0), |(pos, past_crossings), line| {
             let dir = line[0];
-            let dist: i32 = line[1..].to_str().unwrap().parse().unwrap();
+            let dist: i32 = parse_uint(&line[1..]);
 
             if dist == 0 {
                 return (pos, past_crossings);
